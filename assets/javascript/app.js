bfc91f96109ce945225feb6569f7b71506bc51db
Aizhan87 <Aizhan87@aizhans-mbp.home>
2019-06-15 19:22:47 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.sidenav').sidenav();
+    $('.slider').slider();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+        disableWeekends: true
+    });
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+    $('.chart').easyPieChart({
+        //your options goes here
+    });
+});
+
